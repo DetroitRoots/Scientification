19ec55be4651b8c5c8e092fb4a4f4776fc87822c
Goktug Erol <ge_dev@yahoo.com>
2022-03-12 16:11:05 +0300

    Improved menu and minor typo fix.
    Menu improved and made more user friendly. 
    Currently in test. 
    I'm currently working in new feature, the new improvement will be applied to the entire program after the tests and existing tasks.

--- main/systm.c
+++ main/systm.c
@@ -26,7 +26,9 @@ int main()
     printf("GEOMETRIC CALCULATIONS\n");
 	printf("4- to calculate area of circle\n");
 	printf("5- to calculate area of square\n");
-	printf("6 to calculate area of sphere\n");
+	printf("6- to calculate area of sphere\n");
+	printf("7 - TRIGONOMETRIC FUNCTIONS\n"); //testing menu within menu
+
 	choice = input();
 	
 	switch (choice) 
@@ -136,6 +138,37 @@ int main()
 		printf("Area of sphere=");
 		output(result);
 		break;
+	}
+	case 7: //TESTING
+	{ 
+	printf("Choose Function\n");
+	printf("77- Sine\n");
+	printf("-771- Cosine\n");
+	printf("772- Tangent\n");
+	printf("773- Cotangent\n");
+	printf("774- Logarithm\n");
+	printf("775- Raise to a power\n");
+	printf("776- Root Square\n");
+	printf("777- NTH Root\n");
+		int choice2;
+		choice2=input();
+		switch (choice2)
+		{
+		case 77:
+		{
+			printf("It works\n");
+			break;
+		}
+		case 882:
+		{
+			printf("yeah\n");
+			break;
+		}
+		default: printf("Function Complete");
+			break;
+		}
+		break;
+
 	}
 	default:
 		printf("wrong Input\n");
